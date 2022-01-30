f95f76aad303c357d33f29c82dc3f504460b6e27
jo-es <45110941+jo-es@users.noreply.github.com>
2022-01-30 19:46:36 +0100

    Update dev.sh and start.sh

--- dev.sh
+++ dev.sh
@@ -15,7 +15,7 @@ set -o nounset
 
 > state.json
 
-RUST_BACKTRACE=1 RUST_LOG="main,keeper=debug" cargo run -- \
+RUST_BACKTRACE=1 RUST_LOG=debug cargo run -- \
     --config ./addrs.json \
     --private-key ./private_key \
     --url wss://eth-goerli.alchemyapi.io/v2/$ALCHEMY_API_KEY \
@@ -23,4 +23,4 @@ RUST_BACKTRACE=1 RUST_LOG="main,keeper=debug" cargo run -- \
     --interval 7000 \
     --start-block 6142980 \
     --file state.json \
-    --instance-name goerli \
+    --instance-name goerli-dev \
--- start.sh
+++ start.sh
@@ -1,9 +1,29 @@
+#!/usr/bin/env bash
+set -o errexit
+set -o pipefail
+
+if [ -f .env ]; then
+  set -o allexport; source .env; set +o allexport
+fi
+
+if [ -z "$ALCHEMY_API_KEY" ]; then
+  echo "ALCHEMY_API_KEY is undefined in .env";
+  exit 1;
+fi
+
+set -o nounset
+
+> state.json
+
 cargo build --release
 
 # Run it with 
-./target/release/main \
+RUST_BACKTRACE=1 RUST_LOG=info ./target/release/main \
     --config ./addrs.json \
     --private-key ./private_key \
     --url wss://eth-goerli.alchemyapi.io/v2/fe2m2g2zR0VSdfeDvvTlFCVgNycVTybN \
+    --chain-id 5 \
     --interval 7000 \
+    --start-block 6142980 \
     --file state.json \
+    --instance-name goerli \
